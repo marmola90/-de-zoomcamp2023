42bef9688d4e67c20f7c098a9eef23b5ba4f0833
marmola90 <amarmol20@gmail.com>
2023-02-06 15:44:02 -0600

    delete docker

--- gitblock-deployment.py
+++ gitblock-deployment.py
@@ -1,16 +1,16 @@
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
-from prefect.infrastructure.docker import DockerContainer
+#from prefect.infrastructure.docker import DockerContainer
 from etl_web_to_gcs import etl_web_to_gcs
 
 github_block = GitHub.load("zoom-gitbloack")
-docker_block = DockerContainer.load("zoom")
+#docker_block = DockerContainer.load("zoom")
 
 github_dep=Deployment.build_from_flow(
     flow=etl_web_to_gcs,
     name='github_flow',
-    storage=github_block,
-    infrastructure=docker_block
+    storage=github_block
+    # infrastructure=docker_block
 )
 
 
