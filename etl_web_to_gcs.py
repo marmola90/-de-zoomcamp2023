007ae1a41dfa90a97080f0426b30ef98b9e8987f
marmola90 <amarmol20@gmail.com>
2023-02-06 16:55:07 -0600

    change gcs test

--- etl_web_to_gcs.py
+++ etl_web_to_gcs.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import pandas as pd
 from prefect import flow, task
-from prefect_gcp.cloud_storage import GcsBucket
+#from prefect_gcp.cloud_storage import GcsBucket
+from prefect.filesystems import GcsBucket
 
 @task(retries=3)
 def fetch(dataset_url:str)-> pd.DataFrame:
