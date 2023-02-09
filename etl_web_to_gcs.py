3c320d2f3cf6056e20b19aa7d5c5773c9051f9ae
marmola90 <amarmol20@gmail.com>
2023-02-09 15:53:43 -0600

    test1

--- etl_web_to_gcs.py
+++ etl_web_to_gcs.py
@@ -15,7 +15,7 @@ def clean(df=pd.DataFrame)-> pd.DataFrame:
     """Fix dtype issues"""
     df['tpep_pickup_datetime']=pd.to_datetime(df['tpep_pickup_datetime'])
     df['tpep_dropoff_datetime']=pd.to_datetime(df['tpep_dropoff_datetime'])
-    df['passenger_count']=pd.to_numeric(df['passenger_count'],downcast="integer")
+    df['passenger_count']=df['passenger_count'].astype('int')
 
     #df['lpep_pickup_datetime']=pd.to_datetime(df['lpep_pickup_datetime'])
     #df['lpep_dropoff_datetime']=pd.to_datetime(df['lpep_dropoff_datetime'])
